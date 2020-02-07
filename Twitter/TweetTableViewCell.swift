dad58d5e116b17b81d1e37fb9a902218647e57c3
123lenah <123lenah@gmail.com>
2020-02-06 20:09:40 -0800

    // initial commit

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Lenah Syed on 2/6/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
